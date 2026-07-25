9cc42ce3ed64b7c03054820120b7a4acc41284a8
Sujal Gupta <sujal17021@gmail.com>
2026-07-25 18:17:12 +0530

    Debug generate endpoint

--- backend/crud.py
+++ backend/crud.py
@@ -25,7 +25,7 @@ def fetch_dataset(
     rows = (
         db.query(DatasetRow)
         .filter(DatasetRow.dataset_id == dataset_id)
-        .limit(50000)      # <-- TEMPORARY
+        .limit(100000)      # <-- TEMPORARY
         .all()
     )
 
